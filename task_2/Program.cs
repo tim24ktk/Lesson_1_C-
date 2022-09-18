78e5e57ef7760e95c99a99da3853b9206a482e2c
tim24ktk <tim24ktk@mail.ru>
2022-09-18 22:20:28 +0300

    Первое дз C#

---
+++ task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Задача 2
+    
+    Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+    a = 5; b = 7 -> max = 7
+    a = 2 b = 10 -> max = 10
+    a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Пожалуйста введите 1-е число: ");
+
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста введите 2-е число: ");
+
+int secondNum = int.Parse(Console.ReadLine());
+
+if (firstNum > secondNum)
+{
+    Console.WriteLine($"Большее число: {firstNum}, меньшее {secondNum}");
+}
+else
+{
+    Console.WriteLine($"Большее число: {secondNum}, меньшее {firstNum}");
+}
---
+++ task_4/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Задача 4:
+        Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+        2, 3, 7 -> 7
+        44 5 78 -> 78
+        22 3 9 -> 22
+*/
+
+Console.WriteLine("Пожалуйста введите 1-е число: ");
+
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста введите 2-е число: ");
+
+int secondNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста введите 3-е число: ");
+
+int thirdNum = int.Parse(Console.ReadLine());
+
+if (firstNum > secondNum && firstNum > thirdNum)
+{
+    Console.WriteLine(firstNum);
+}
+else if (secondNum > thirdNum)
+{
+    Console.WriteLine(secondNum);
+}
+else
+{
+    Console.WriteLine(thirdNum);
+}
